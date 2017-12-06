f3ce45c3df80df7f69b6c1d5fde89cd831559586
mahdi hajian <31277401+mahdi-hajian@users.noreply.github.com>
2017-12-06 09:59:23 +0330

    cteate DataBase

--- EntityFramework/Form1.cs
+++ EntityFramework/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.Entity;
+using EntityFramework.Models;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,34 @@ namespace EntityFramework
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataBaseContext oDataBaseContext = null;
+            try
+            {
+                oDataBaseContext = new DataBaseContext();
+                Country oCountry = new Country()
+                {
+                    Id = 1,
+                    Name = "ایران",
+                };
+                oDataBaseContext.Countries.Add(oCountry);
+                oDataBaseContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (oDataBaseContext != null)
+                {
+                    oDataBaseContext.Dispose();
+                    oDataBaseContext = null;
+                }
+            }
         }
     }
 }
---
+++ EntityFramework/Models/DataBaseContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.Entity;
+using System.Collections.Generic;
+
+namespace EntityFramework.Models
+{
+    public class DataBaseContext:DbContext
+    {
+        private DbSet<Country> _countries;
+
+        public DbSet<Country> Countries
+        {
+            get { return _countries; }
+            set { _countries = value; }
+        }
+        static DataBaseContext()
+        {
+            //اگر مدل تغییر کرد دوباره ساخته میشود
+            //یعنی اگر یک کلاس اضافه شد یا یک پراپرتی تغییر کرد یا اضافه شد یا ...
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<DataBaseContext>());
+
+            //وقتی برنامه ران میشود دوباره دیتابیس ساخته میشود
+            //Database.SetInitializer(new DropCreateDatabaseAlways<DataBaseContext>());
+
+            //اگر دیتابیس وجود نداشت ان را میسازد
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<DataBaseContext>());
+        }
+        public DataBaseContext():base("aa")
+        {
+
+        }
+    }
+}
